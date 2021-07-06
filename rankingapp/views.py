5207362a9e5745c63bdf5f5f4b44b4658b5bcf2e
Hanseung Lee <hs.lee@haezoom.com>
2021-07-06 16:10:26 +0900

    ranking order added

--- rankingapp/views.py
+++ rankingapp/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .serializers import RankingSerializer
 from .models import Ranking
+import datetime
 
 class RankingViewset(viewsets.ModelViewSet):
-    queryset = Ranking.objects.all()
+    today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+    today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+
+    #queryset = Ranking.objects.filter(created__range=(today_min, today_max)).order_by('-score')
+    queryset = Ranking.objects.all().order_by('-score')
+
     serializer_class = RankingSerializer
