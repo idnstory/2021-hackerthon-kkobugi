e99cb3b4466e92bbc46965e235820c643ac3e940
Hanseung Lee <hs.lee@haezoom.com>
2021-07-06 16:59:04 +0900

    Vue test file test.html added

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -4,5 +4,7 @@ from helloworld import views
                                                                                                                        
 urlpatterns = [                                                                                                        
     path('', views.hello, name='hello'),                                                                               
+    path('index', views.index, name='index'),                                                                       
     path('camera', views.camera, name='camera'),                                                                       
+    path('test', views.test, name='test'),                                                                       
 ]                 
--- helloworld/views.py
+++ helloworld/views.py
@@ -7,4 +7,10 @@ def hello(request):
     return HttpResponse("hello")
 
 def camera(request):
+    return render(request, "camera.html")
+
+def index(request):
     return render(request, "index.html")
+
+def test(request):
+    return render(request, "test.html")
