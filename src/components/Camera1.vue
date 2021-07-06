933aadb790e6e98358dc9310a162b5aa387e2022
하유리 <gkdbfl1004@gmail.com>
2021-07-06 16:29:10 +0900

    camera에 필요한 npm 설치

--- src/components/Camera1.vue
+++ src/components/Camera1.vue
@@ -6,9 +6,9 @@
   </div>
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@teachablemachine/pose@0.8/dist/teachablemachine-pose.min.js"></script>
 <script>
+import * as tf from '@tensorflow/tfjs';
+import * as tmPose from '@teachablemachine/pose';
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
 const URL = "../ml_files/5pose0706mlmodel/";
@@ -54,48 +54,11 @@ export default {
     await predict();
     window.requestAnimationFrame(loop);
   },
-
-predict: async function() {
-  //계속돌면서분류모델
-  // Prediction #1: run input through posenet
-  // estimatePose can take in an image, video or canvas html element
-  const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
-  // Prediction 2: run input through teachable machine classification model
-  const prediction = await model.predict(posenetOutput);
-
-  var maxValueClassName;
-  var maxValue = 0;
-  for (let i = 0; i < maxPredictions; i++) {
-    const classPrediction =
-      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-    labelContainer.childNodes[i].innerHTML = classPrediction;
-    if (maxValue < prediction[i].probability) {
-      maxValue = prediction[i].probability;
-      maxValueClassName = prediction[i].className;
-    }
-  }
-
-  const classified = "classified";
-  console.log(maxValueClassName);
-  localStorage.setItem(classified, maxValueClassName); //담아준다.
-
-  // finally draw the poses
-  drawPose(pose);
-},
-
-  drawPose(pose) {
-  if (webcam.canvas) {
-    ctx.drawImage(webcam.canvas, 0, 0);
-    // draw the keypoints and skeleton
-    if (pose) {
-      const minPartConfidence = 0.5;
-      tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-      tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-      }
-    }
-  }
   },
   computed: {},
+  watch: {
+
+  },
   mounted() {
 
   }
